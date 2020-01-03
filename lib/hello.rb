e0f74a2450a2f7a983243486dc159337186873e0
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-03 00:07:51 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,9 @@
 def hello_t(array)
-  
+  i = 0 
+  while i < array.length 
+    yield array[i] #essentially it is iterating through and doing the stuff outside
+    i = i + 1 #iterates
+  end
 end
 
 # call your method here!
